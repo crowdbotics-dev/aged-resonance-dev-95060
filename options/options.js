26fb74059cd8cb053554dbac6d818b627d90571f
Crowdbotics <engineering@crowdbotics.com>
2023-06-27 06:38:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-resonance-dev-95060",
+  url: "https://aged-resonance-dev-95060.botics.co",
+  api: "https://aged-resonance-dev-95060.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
